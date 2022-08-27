eb171f46d121ea956ffeb0fa9da4e172fd32fbfb
Andres M. Prieto <andresprieto2703@gmail.com>
2022-08-27 12:59:43 -0500

    Quick fix

--- src/simulator/index.js
+++ src/simulator/index.js
@@ -1,13 +1,15 @@
-import { fillUpToZero, random } from '../util/index';
+import { fillUpToZero, random } from '../util';
 
 const cost = [9.0, 5.0, 12.0, 6.0, 4.0, 10.0, 5.0, 14.0, 3.0, 8.0];
 const volume = [10.0, 6.0, 8.0, 4.0, 9.0, 4.0, 10.0, 3.0, 7.0, 8.0];
 const costOverVolume = [0.9, 0.83, 1.5, 1.5, 0.44, 2.5, 0.5, 4.67, 0.43, 1.0];
 const k_factor = [9.2, 5.2, 11.2, 5.6, 5.0, 8.8, 6.0, 11.8, 3.8, 8.0];
 
-let current = [];
+let schema = [];
 
 const simulate = () => {
-  current[random(0, cost.length)] = 1;
-  console.log(current);
+  schema[random(0, cost.length)] = 1;
+  console.log(schema);
 };
+
+simulate()
