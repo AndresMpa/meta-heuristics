6eff5cac2efd81ebcfc85de3415bcf65d8ed3137
Andres M. Prieto <andresprieto2703@gmail.com>
2022-08-27 09:34:28 -0500

    Example
    Quick example

---
+++ src/simulator/index.js
@@ -0,0 +1,13 @@
+import { fillUpToZero, random } from '../util/index';
+
+const cost = [9.0, 5.0, 12.0, 6.0, 4.0, 10.0, 5.0, 14.0, 3.0, 8.0];
+const volume = [10.0, 6.0, 8.0, 4.0, 9.0, 4.0, 10.0, 3.0, 7.0, 8.0];
+const costOverVolume = [0.9, 0.83, 1.5, 1.5, 0.44, 2.5, 0.5, 4.67, 0.43, 1.0];
+const k_factor = [9.2, 5.2, 11.2, 5.6, 5.0, 8.8, 6.0, 11.8, 3.8, 8.0];
+
+let current = [];
+
+const simulate = () => {
+  current[random(0, cost.length)] = 1;
+  console.log(current);
+};
