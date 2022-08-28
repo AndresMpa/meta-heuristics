493bcd2036967e0dc7820707bc9f42abcd35b86d
Andres M. Prieto <andresprieto2703@gmail.com>
2022-08-28 14:01:05 -0500

    Single responsibility
    Using single responsibility principle

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
-const random = (min, max) => Math.floor(Math.random() * (max - min)) + min;
-
-const fillUpToZero = (data) => {
-  cost.forEach((_) => {
-    data.push(0);
-  });
-};
-
-const cost = [9.0, 5.0, 12.0, 6.0, 4.0, 10.0, 5.0, 14.0, 3.0, 8.0];
-const volume = [10.0, 6.0, 8.0, 4.0, 9.0, 4.0, 10.0, 3.0, 7.0, 8.0];
-const costOverVolume = [0.9, 0.83, 1.5, 1.5, 0.44, 2.5, 0.5, 4.67, 0.43, 1.0];
-const k_factor = [9.2, 5.2, 11.2, 5.6, 5.0, 8.8, 6.0, 11.8, 3.8, 8.0];
+import { fillUpToZero, getSchema } from './util/helpers.js';
+import { getSample } from './dataHandlers/store.js';
 
 let schema = [];
 
 const simulate = () => {
-  fillUpToZero(schema)
-  schema[random(0, cost.length)] = 1;
-  console.log(schema);
+  const data = getSample();
+  fillUpToZero(data['cost'], schema);
+  getSchema(data, schema, "k");
+  console.log(schema)
 };
 
-simulate()
+simulate();
