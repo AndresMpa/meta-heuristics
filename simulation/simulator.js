af7929a7ada320497872219064e303ed3d5e43ef
Andres M. Prieto <andresprieto2703@gmail.com>
2022-08-29 15:35:38 -0500

    Split
    Keeping index as simple as possible

---
+++ simulation/simulator.js
@@ -0,0 +1,115 @@
+import { getSchema, updateVolume } from '../schema/generator.js';
+import { generateNeighborhood } from '../schema/neighbour.js';
+import { isZero, fillUpToZero } from '../util/helpers.js';
+import { makeFile } from '../dataHandlers/fileHandler.js';
+import { getSample } from '../dataHandlers/store.js';
+import { cliHandler } from '../cli/handler.js';
+
+const results = (simulation, result, iterations, time, options) => {
+  console.group('----------------Simulation results----------------');
+  console.group('Results:');
+  console.group(`Simulation:`);
+  console.log(`Simulation got a max cost of: ${result.cost}`);
+  console.log(`Simulation got a min volume of: ${result.volume}`);
+  console.log(`Simulation fill up limit volume of: ${result.limitVolume}`);
+  console.log(`Simulation used methods:`);
+  console.log(result.methods);
+  console.log('Simulation got schema:');
+  console.log(result.schema);
+  console.groupEnd('Simulation got:');
+  console.groupEnd('Results:');
+
+  console.group('Performance');
+  console.group(`Simulation took:`);
+  console.log(`${time} miliseconds`);
+  console.log(`${iterations} iterations`);
+  console.groupEnd('Simulation took:');
+  console.groupEnd('Performance');
+
+  console.group('Extras');
+  console.log(
+    `Simulation terminated due to factiblility: ${simulation.factible}`
+  );
+
+  console.group(`Last neighbour:`);
+  console.log(`Neighbour suggested a cost of: ${simulation.cost}`);
+  console.log(`Neighbour suggested a volume of: ${simulation.volume}`);
+  console.log(
+    `Neighbour used method: ${
+      simulation.methods[simulation.methods.length - 1]
+    }`
+  );
+  console.log('Neighbour suggested schema:');
+  console.log(simulation.schema);
+  console.groupEnd('Last neighbour:');
+
+  if (options.keep) {
+    console.log(`Find extra log information on ./logs/${options.id}.json`);
+  }
+  console.groupEnd('Extra');
+
+  console.groupEnd('----------------Simulation result----------------');
+};
+
+const simulate = (simulation, iterations) => {
+  const data = getSample();
+  const options = cliHandler();
+
+  const timeStart = performance.now();
+
+  fillUpToZero(data['cost'], simulation['schema']);
+  updateVolume(data, simulation);
+
+  getSchema(data, simulation, options['schema'][0]);
+
+  console.log(`Iteration ${simulation['methods'].length - 1}: Schema`);
+  console.log(simulation);
+
+  iterations.push(simulation);
+
+  while (
+    simulation['limitVolume'] >= simulation['volume'] &&
+    isZero(simulation['schema'])
+  ) {
+    console.log(`Iteration ${iterations.length}: `);
+    console.log('State of data:');
+    console.log(data);
+
+    if (options.keep) {
+      iterations.push([
+        structuredClone(data),
+        structuredClone(generateNeighborhood(data, simulation)),
+      ]);
+    } else {
+      iterations.push(structuredClone(generateNeighborhood(data, simulation)));
+    }
+  }
+
+  console.log('Simulation terminated');
+
+  const timeEnd = performance.now();
+
+  if (options.keep) {
+    results(
+      simulation,
+      iterations[iterations.length - 2][1],
+      iterations.length,
+      timeEnd - timeStart,
+      options
+    );
+  } else {
+    results(
+      simulation,
+      iterations[iterations.length - 2],
+      iterations.length,
+      timeEnd - timeStart,
+      options
+    );
+  }
+
+  if (options.keep) {
+    makeFile(JSON.stringify(iterations), 'json', 'logs/', '.', options.id);
+  }
+};
+
+export { simulate };
