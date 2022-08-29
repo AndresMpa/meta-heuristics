9f47fc5845b9d45bab470652e366fa91448b80b5
Andres M. Prieto <andresprieto2703@gmail.com>
2022-08-28 19:07:43 -0500

    Logs
    Login what happen on a terminal

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
+import { getSchema, updateVolume } from './schema/generator.js';
+import { generateNeighborhood } from './schema/simulation.js';
 import { getSample } from './dataHandlers/store.js';
-import { getSchema } from './schema/generator.js';
 import { fillUpToZero } from './util/helpers.js';
 import { cliHandler } from './cli/handler.js';
 
+let iterations = [];
 let simulation = {
   factible: true,
   methods: [],
   schema: [],
 
+  limitVolume: 0,
   volume: 0,
   cost: 0,
 };
@@ -17,9 +20,21 @@ const simulate = () => {
   const options = cliHandler();
 
   fillUpToZero(data['cost'], simulation['schema']);
-  getSchema(data, simulation, options['shema']);
+  updateVolume(data, simulation);
 
+  getSchema(data, simulation, options['schema'][0]);
+
+  console.log(`Iteration ${simulation['methods'].length - 1}: Schema`);
   console.log(simulation);
+
+  iterations.push(simulation);
+
+  while (simulation['limitVolume'] >= simulation['volume']) {
+    console.log(`Iteration ${simulation['methods'].length}: `);
+    generateNeighborhood(data, simulation);
+
+    iterations.push(simulation);
+  }
 };
 
 simulate();
