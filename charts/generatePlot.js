e30691a752d0d5b1d0452c21c854b044b5a8fdca
Andres M. Prieto <andresprieto2703@gmail.com>
2022-10-21 13:01:15 -0500

    Charts
    Adding charts module

---
+++ charts/generatePlot.js
@@ -0,0 +1,14 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
+// It generates a plot using python module
+const shellExec = require('child_process').spawn;
+const process = shellExec('python', ['./charts/plot.py']);
+
+// Takes stdout data from script which executed
+// with arguments and send this data to res object
+process.stdout.on('data', () => console.log('Image created'));
+console.log(process)
+
+
