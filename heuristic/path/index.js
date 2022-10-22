632322df127f5b6344c0e91a0c306bcdfd50e563
Andres M. Prieto <andresprieto2703@gmail.com>
2022-10-22 16:22:48 -0500

    Logger
    Using a better log system

--- heuristic/path/index.js
+++ heuristic/path/index.js
@@ -3,6 +3,7 @@ import { getInitialSchema } from '../../schema/generator.js';
 import { generateNeighborhood } from './neighbour.js';
 // Utilities
 import { pathResults } from '../../util/information.js';
+import { getProcessData } from '../../util/process.js';
 import { isZero } from '../../util/helpers.js';
 // Data handlers
 import { makeFile } from '../../dataHandlers/fileHandler.js';
@@ -10,15 +11,15 @@ import { makeFile } from '../../dataHandlers/fileHandler.js';
 /*
   It generates a simulation for process epochs
 */
-const pathSimulation = (simulation, iterations) => {
-  const initialSchema = getInitialSchema(simulation);
-  const data = initialSchema[0];
-  const options = initialSchema[1];
-
+const pathSimulation = (simulation, iterations, epoch, options) => {
   const timeStart = performance.now();
+  const initialSchema = getInitialSchema(simulation, options);
+  const data = initialSchema;
 
-  console.log(`Iteration ${simulation['methods'].length - 1}: Schema`);
-  console.log(simulation);
+  if (getProcessData().LOGGER === '1') {
+    console.log(`Iteration ${simulation['methods'].length - 1}: Schema`);
+    console.log(simulation);
+  }
 
   iterations.push(structuredClone(simulation));
 
@@ -26,9 +27,11 @@ const pathSimulation = (simulation, iterations) => {
     simulation['limitVolume'] >= simulation['volume'] &&
     isZero(simulation['schema'])
   ) {
-    console.log(`Iteration ${iterations.length}: `);
-    console.log('State of data:');
-    console.log(data);
+    if (getProcessData().LOGGER === '1') {
+      console.log(`Iteration ${iterations.length}: `);
+      console.log('State of data:');
+      console.log(data);
+    }
 
     if (options.keep) {
       iterations.push([
@@ -40,28 +43,47 @@ const pathSimulation = (simulation, iterations) => {
     }
   }
 
-  console.log('\n---------------Simulation terminated--------------\n');
+  if (getProcessData().LOGGER === '1') {
+    console.log('\n---------------Simulation terminated--------------\n');
+  }
+
+  if (iterations.length === 1) {
+    iterations.push(iterations[0]);
+  }
 
   const timeEnd = performance.now();
 
   if (options.keep) {
-    pathResults(
-      simulation,
-      iterations[iterations.length - 2][1],
-      iterations.length,
-      timeEnd - timeStart,
-      options
-    );
+    if (getProcessData().LOGGER === '1') {
+      pathResults(
+        simulation,
+        iterations[iterations.length - 2][1],
+        iterations.length,
+        timeEnd - timeStart,
+        epoch,
+        options
+      );
+    }
 
-    makeFile(JSON.stringify(iterations), 'json', 'logs/', '.', options.id);
-  } else {
-    pathResults(
-      simulation,
-      iterations[iterations.length - 2],
-      iterations.length,
-      timeEnd - timeStart,
-      options
+    makeFile(
+      JSON.stringify(iterations),
+      epoch,
+      options.id,
+      'json',
+      'logs/',
+      '.'
     );
+  } else {
+    if (getProcessData().LOGGER === '1') {
+      pathResults(
+        simulation,
+        iterations[iterations.length - 2],
+        iterations.length,
+        timeEnd - timeStart,
+        epoch,
+        options
+      );
+    }
   }
 };
 
