7e610497060436467c1ec4dee4809560a0955962
Andres M. Prieto <andresprieto2703@gmail.com>
2022-10-23 00:27:34 -0500

    Genetics
    Adding genetics basics

--- heuristic/genetic/index.js
+++ heuristic/genetic/index.js
@@ -1,13 +1,10 @@
 // Schemes handlers
-import { getInitialSchema } from '../../schema/generator.js';
-// Utilities
-import { isZero } from '../../util/helpers.js';
-//import { results } from '../../util/information.js';
+import { getInitialPopulation } from '../../population/generator.js';
 // Data handlers
 import { makeFile } from '../../dataHandlers/fileHandler.js';
 
-const geneticSimulation = (simulation, iterations) => {
-  const data = getInitialSchema(simulation)
+const geneticSimulation = (simulation, iterations, options) => {
+  const genotype = getInitialPopulation(simulation, options);
 
   const timeStart = performance.now();
   const timeEnd = performance.now();
