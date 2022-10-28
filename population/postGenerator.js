de112f0330b0a48d760f6070bbacf302871f873d
Andres M. Prieto <andresprieto2703@gmail.com>
2022-10-28 00:43:47 -0500

    Utility
    Adding some utilities to get best set of individuals

--- population/postGenerator.js
+++ population/postGenerator.js
@@ -1,5 +1,25 @@
-const handleGeneratedPopulation = (data, simulation)=> {
-    console.log(Math.max(...simulation['cost']));
-}
+const handleGeneratedPopulation = (data, simulation, indexes) => {
+  let maxCost = Math.max(...simulation['cost']);
+  let index = simulation['cost'].indexOf(maxCost);
 
-export { handleGeneratedPopulation }
+  if (simulation['factible'][index]) {
+    indexes.push(index);
+  }
+  simulation['cost'].splice(index, 1);
+
+  if (indexes.length > 2) {
+    return indexes;
+  }
+
+  return handleGeneratedPopulation(data, simulation, indexes);
+};
+
+const updateGreatest = (greatest, simulation) => {
+  //console.log(greatest);
+  //console.log(simulation);
+    greatest.forEach((single) => {
+        //simulation[]
+    })
+};
+
+export { handleGeneratedPopulation, updateGreatest };
