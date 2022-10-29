c95f57e6996b43542c69c81900c48f1bdf730871
Andres M. Prieto <andresprieto2703@gmail.com>
2022-10-29 18:41:54 -0500

    Documentation
    Using Entrance Process Output (EPO) orientation to make this less
    painful

--- heuristic/genetic/index.js
+++ heuristic/genetic/index.js
@@ -17,12 +17,23 @@ const running = (current) =>
 const geneticSimulation = (population, generations, epoch, options) => {
   const timeStart = performance.now();
 
+  /*
+    Initial population generation
+    Create the first population (Equivalent to create a species)
+      E -> Process data
+      P -> Create a population
+      O -> Initial population
+  */
   const populationData = getInitialPopulation(population);
   const greatestIndividuals = getGreatestIndividuals(
     populationData,
     population
   );
-  calculateGreatestCharacteristics(populationData, population, greatestIndividuals);
+  calculateGreatestCharacteristics(
+    populationData,
+    population,
+    greatestIndividuals
+  );
 
   generations.push([populationData, population, greatestIndividuals]);
 
@@ -31,6 +42,30 @@ const geneticSimulation = (population, generations, epoch, options) => {
   }
 
   while (running(generations.length)) {
+    /*
+      Selection
+      Choose which individuals will used for the next generation
+        E -> Previous population
+        P -> Choose individuals
+        O -> List of individuals
+    */
+
+    /*
+      Recombination
+      Creates a new population from a previous one
+        E -> Previous population, Chosen individuals
+        P -> Create a new population
+        O -> New population
+    */
+
+    /*
+      Mutation
+      Change some gens to mutate individuals
+        E -> New population
+        P -> Alter some individuals
+        O -> Population with some mutations
+    */
+
     if (getProcessData().LOGGER === '1') {
       geneticInteration(generations);
     }
