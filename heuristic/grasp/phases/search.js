aff7271a38566e59fa9688554a48b90abc080675
Andres Prieto <andresprieto2703@gmail.com>
2022-11-10 07:44:15 -0500

    GRASP
    Basic scheme for GRASP

---
+++ heuristic/grasp/phases/search.js
@@ -0,0 +1,10 @@
+/**
+ * This phase handle with search process, it
+ * choose a random neighbour from a neighborhood
+ */
+const searchPhase = (neighborhood) => {
+  const neighbour = [];
+  return neighbour;
+};
+
+export { searchPhase };
